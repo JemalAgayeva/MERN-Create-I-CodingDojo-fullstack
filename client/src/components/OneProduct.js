c1a980d4fe89604c4bc9b4900d7ea7358969c404
Jemal Agayeva <Jemal.agayeva@gmail.com>
2021-03-25 00:08:59 -0400

    CRUD - Creat is complete. Delete is integrated.

--- client/src/components/OneProduct.js
+++ client/src/components/OneProduct.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import {navigate} from '@reach/router'
 
 const OneProduct = (props) => {
 
@@ -16,6 +17,17 @@ const OneProduct = (props) => {
 
 }, [])
 
+const deleteProduct = (e, productId) => {
+    console.log("deleting product", productId)
+    axios.delete(`http://localhost:8000/api/projects/delete/${props.productId}`)
+        .then(response => {
+            console.log("deleted")
+            console.log(response)
+            navigate("/")
+        })
+        .catch(err => console.log(err))
+}
+
     return (
         <div className="card">
             <div className="card-body">
@@ -23,6 +35,7 @@ const OneProduct = (props) => {
                 <p className="card-text font-weight-bolder">Price: ${oneProduct.price}</p>
                 <p className="card-text text-secondary">Description: {oneProduct.description}</p>
                 {/* <a href="#!" className="btn btn-primary">Go somewhere</a> */}
+            <button onClick={(e)=> deleteProduct(e, oneProduct._id)} className="btn btn-danger">Delete Product</button>
             </div>
         </div>
     );
